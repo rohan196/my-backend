147d4f0673a902eb98aba0fc56afb92d3ac5b28a
Rohan <rajputrohan196@gmail.com>
2024-10-21 10:12:46 +0530

    Controllers completed

--- src/app.js
+++ src/app.js
@@ -9,18 +9,34 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json({limit: "16kb"}))                                  // To accept json data
-app.use(express.urlencoded({extended: true, limit: "16kb"}))            // To accept and understand url data
+app.use(express.json({limit: "50mb"}))                                  // To accept json data
+app.use(express.urlencoded({extended: true, limit: "50mb"}))            // To accept and understand url data
 app.use(express.static('public'))                                // To make a public folder on our server to keep files and folders
 app.use(cookieParser())                                                 // To access user browser cookies
 
-// routes import 
-import userRouter from './routes/user.routes.js';
+//routes import
 
-// routes declaration
-app.use('/api/v1/users', userRouter);
+import userRouter from "./routes/user.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
-// http:localhost:8000/api/v1/users/register
+//routes declaration
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/tweet", tweetRouter);
+app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+
 
 
 export {app} 
